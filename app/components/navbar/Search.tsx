d8564df36be8ef4cc679bd769121301f6ab1072e
Kobe <kobetbuckley@gmail.com>
2023-05-28 10:57:59 -1000

    replaced logo

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,6 +1,43 @@
+'use client'
+
 const Search = () => {
     return (
-        <div>
+        <div
+          className="
+            border-[1px]
+            w-full
+            md:w-auto
+            py-2
+            rounded-full
+            shadow-sm
+            hover:shadow-md
+            transition
+            cursor-pointer       
+          "
+        >
+            <div
+              className="
+                flex
+                flex-row
+                items-center
+                justify-between
+              
+              "
+            
+            >
+                <div
+                className="
+                  text-sm
+                  font-semibold
+                  px-6
+                "
+                
+                >
+                    Any K-pop Group
+
+                </div>
+
+            </div>
 
         </div>
     )
